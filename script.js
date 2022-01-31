2c7115946167ed1596e7846586aa1f4e22cbb2d7
mummj <mummj@eou.edu>
2022-01-31 09:56:17 -0800

    added consructor function

--- script.js
+++ script.js
@@ -19,3 +19,20 @@ console.log(spaceship.flying());
 
 document.write("<h2>", spaceship.flying(), "</h2>")
 document.write("<h2> My spaceship is ", spaceship.color, " and has a ", spaceship.room, " for style and comfomort! <h2>")
+
+function Icecream (flavor, dish, size){
+  this.flavor = flavor;
+  this.dish = dish;
+  this.size = size;
+}
+const jaidynsIcecream = new Icecream ('coffee', 'cone', 'small');
+
+const perlasIcecream = new Icecream ('carmel', 'cone', 'medium');
+
+Icecream.prototype.toppings = this.toppings;
+
+const tyisIcecream = new Icecream ('cookie dough', 'bowl', 'medium',);
+
+console.log(jaidynsIcecream);
+console.log(perlasIcecream);
+console.log(tyisIcecream);
